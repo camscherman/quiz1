48e26938896a4d9086c28c6f671640358479f7f6
Cam Scherman <camscherman@hotmail.com>
2017-10-10 10:18:02 -0700

    Add db:migrate script, perform migration to make sure everything is working

---
+++ db/migration.js
@@ -0,0 +1,19 @@
+const kx = require('./connection')
+// Use Knex to a create table to hold clucks, Cluckr's equivalent of tweets. Clucks should have the following fields:
+// 
+// A username
+// An image_path
+// A content
+// A self- increment unique id
+// created_at and updated_at timestamps
+
+kx.schema.createTable('clucks', table =>{
+    table.increments('id')
+    table.string('username')
+    table.text('content')
+    table.string('image_path')
+    table.timestamps(false, true)
+}).then(()=> process.exit())
+    .catch(()=> process.exit())
+
+module.exports = kx    
