b13bfb4f98844d63981c983c3b91d289e50f10d7
Cam Scherman <camscherman@hotmail.com>
2017-10-10 12:01:09 -0700

    Fix problem with image upload

--- routes/clucks.js
+++ routes/clucks.js
@@ -7,23 +7,21 @@ const multer = require('multer')
 const upload = multer({dest: path.join(__dirname, '..', 'public', 'uploads')})
 
 
-router.get('/', (req, res) =>{
-    res.send("Clucks go here")
-})
+
 router.get('/new', (req, res) =>{
     res.render('./clucks/new')
 })
 
-router.get('/test', (req, res) =>{
+router.get('/', (req, res) =>{
     kx.select().from('clucks').then( clucks => res.send(clucks))
 })
 router.post('/new', upload.single('photo'), (req, res) => {
     const {username,content} = req.body
-    const {file_path} = req.file
-    kx.insert({username: username, content: content, image_path: file_path})
+    const {filename} = req.file
+    kx.insert({username: username, content: content, image_path: `/uploads/${filename}`})
     .into('clucks')
     .then(()=>{
-        res.redirect('/clucks/test')
+        res.redirect('/clucks')
     })
     
 })
